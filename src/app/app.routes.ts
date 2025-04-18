a8152416d9062bd46a2248286f8fded6044c84c9
beya <beyabenyedder33@gmail.com>
2025-04-18 17:32:46 +0200

    authwithprb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children: [
+          { path: 'login', component: LoginComponent },
+          { path: 'register', component: RegisterComponent },
+          { path: 'reset-password', component: ResetPasswordComponent }
+        ]
+      },
+    { path:'', redirectTo: '/auth', pathMatch:'full'}
+];
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  name = '';
+  email = '';
+  password = '';
+
+  onRegister() {
+    console.log('Register with', this.name, this.email, this.password);
+  }
+}
